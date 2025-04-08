a4b6c0462f1ddc796171011cc0f6e85dc0bf0186
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2025-04-08 16:47:29 +1000

    using .env for local and secrets for github actions

--- src/test/java/com/api/data/Credentials.java
+++ src/test/java/com/api/data/Credentials.java
@@ -1,7 +1,23 @@
 package com.api.data;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class Credentials {
-    public static final String USERNAME = "d203";
-    public static final String PASSWORD = "anoPwd@79999";
+    public static final String USERNAME = getEnvVar("LOGIN_USERNAME");
+    public static final String PASSWORD = getEnvVar("LOGIN_PASSWORD");
     public static final boolean REMEMBER_ME = false;
+
+    private static String getEnvVar(String key) {
+        String fromSystem = System.getenv(key);
+        if (fromSystem != null) return fromSystem;
+
+        // fallback to .env file
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        String fromDotenv = dotenv.get(key);
+        if (fromDotenv != null) return fromDotenv;
+
+        throw new RuntimeException("Missing required environment variable: " + key);
+    }
 }
+
+
