56bcfb689e5bd08eaae9c2d39b22206672f3572a
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2025-04-08 15:25:37 +1000

    allure report will generate even if test fails

--- src/test/java/com/api/data/Url.java
+++ src/test/java/com/api/data/Url.java
@@ -2,7 +2,7 @@ package com.api.data;
 
 public class Url {
     //public static final String BASE_URL = "http://localhost:8000";
-    public static final String BASE_URL = "http://porticotennis.link";
+    public static final String BASE_URL = "https://porticotennis.link";
     public static final String auth_login = "/auth/login";
     public static final String bookings_populate_start_times = "/bookings/populate-start-times";
 }
